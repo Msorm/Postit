154859b81367f3eaa027b978ec70e28244df5c7e
Michael Sorm <michaelsorm12@gmail.com>
2014-06-13 20:34:04 +1200

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :update, :show] 
+
+
   def index
   	@posts = Post.all
   end
 
   def show
-  	@post = Post.find(params[:id])
   end
 
   def new
-  	
+  	@post = Post.new
   end
 
   def create
-  	
+  	@post = Post.new(post_params)
+    @post.user = User.first
+
+    if @post.save
+      flash[:notice] = "Your post has been saved"
+      redirect_to post_path
+    else
+      render :new
+    end
   end
 
   def edit
-  	
   end
 
   def update
-  	
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated"
+      redirect_to post_path(@post)     
+
+     else
+     render :edit
+     end 
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
